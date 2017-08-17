12905bfc77265701b0d95b57a864ab8f57b1040b
Sam Kington <github@illuminated.co.uk>
2017-08-17 14:34:49 +0200

    Documentation clarification.

--- lib/Data/Tersify.pm
+++ lib/Data/Tersify.pm
@@ -35,19 +35,19 @@ you don't fully understand and you have a variable you want to inspect,
 and you say C<x $foo> in the debugger, or C<print STDERR Dumper($foo)> from
 your code, or something very similar with the dumper module of your choice,
 and you then get I<pages upon pages of unhelpful stuff> because C<$foo>
-contained a reference to a DateTime, DBIx::Class, Moose or other verbose
-object... you didn't need that.
+contained, I<somewhere> a reference to a DateTime, DBIx::Class, Moose or other
+verbose object... you didn't need that.
 
 Data::Tersify looks at any data structure it's given, and if it finds a
-blessed object that it knows about, it replaces it in the data structure
-by a terser equivalent, designed to (a) not use up all of your scrollback,
-but (b) be blatantly clear that this is I<not> the original object that
-was in that data structure originally, but a terser equivalent.
+blessed object that it knows about, anywhere, it replaces it in the data
+structure by a terser equivalent, designed to (a) not use up all of your
+scrollback, but (b) be blatantly clear that this is I<not> the original object
+that was in that data structure originally, but a terser equivalent.
 
 Do not use Data::Tersify as part of any serialisation implementation! By
-design, Data::Tersify is lossy and will throw away information! It supposes,
-though, that if you're using it, you want to dump information about a complex
-data structure, and you don't I<care> about the fine details.
+design, Data::Tersify is lossy and will throw away information! That's because
+it supposes that that if you're using it, you want to dump information about a
+complex data structure, and you don't I<care> about the fine details.
 
 =head2 tersify
 
